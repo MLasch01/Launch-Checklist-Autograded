88c6820f148ba86e71537500caa2363f61c44f71
mlasch01 <mark.lasch@yahoo.com>
2024-08-25 08:44:09 -0500

    Finished

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Write your JavaScript code here!
 
-const { formSubmission, validateInput, pickPlanet } = require("./scriptHelper");
+// const { formSubmission, validateInput, pickPlanet } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
-        // form.getElementById("formSubmit").addEventListener("submit", validateInput);
-        let form = document.querySelector("form");
-        alert("Form loaded");
+
+
         let listedPlanets;
         // Set listedPlanetsResponse equal to the value returned by calling myFetch()
         let listedPlanetsResponse = myFetch();
@@ -14,49 +13,31 @@ window.addEventListener("load", function() {
             console.log(listedPlanets);
         }).then(function () {
             console.log(listedPlanets);
-        });
-        let RandomPlanet = pickPlanet(listedPlanetsResponse);
-        let name = RandomPlanet["name"];
-        let diameter = RandomPlanet["diameter"];
-        let star = RandomPlanet["star"];
-        let distance = RandomPlanet["distance"];
-        let moons = RandomPlanet["moons"];
-        let imageUrl = RandomPlanet["imageUrl"];
-        addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl)
+
+        let selectedPlanet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, 
+            selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.imageUrl)
+        });  
+
+
+        let form = document.getElementById("launchForm");
+        let list = document.getElementById("faultyItems");
 
          // Below this comment call the appropriate helper functions to pick a planet from the list of planets 
          // and add that information to your destination.
  
         form.addEventListener("submit", function(event){
+            
+            event.preventDefault();
+
             let pilotName = document.querySelector("input[name=pilotName]");
+            let pilot = pilotName.value;
             let copilotName = document.querySelector("input[name=copilotName]");
+            let copilot = copilotName.value;
             let fuelLevel = document.querySelector("input[name=fuelLevel]");
+            let fuel = fuelLevel.value;
             let cargoMass = document.querySelector("input[name=cargoMass");
-
-            let fuelLevelValid = validateInput(fuelLevel);
-            let cargoLevelValid = validateInput(cargoLevel);
-
-            if(validateInput(pilot) == "Empty" || validateInput(copilot) == "Empty" ||
-                fuelLevelValid == "Empty" || cargoLevelValid == "Empty"){
-                alert("All fields are required!");
-            event.preventDefault();
-            } else if (fuelLevelValid == "Not a number" || cargoLevelValid == "Not a number"){
-            alert("Fuel level and cargo level must be numbers")
-            event.preventDefault();
-            }
-
-            formSubmission(document, listedPlanetsResponse, pilotName, copilotName, fuelLevel, cargoMass);
-            console.log("PilotName: " + pilotName);
-
+            let cargoLevel = cargoMass.value;
+            formSubmission(document, list, pilot, copilot, fuel, cargoLevel);
         });
-        
     });
-    
-
-    
-       
- 
-        
-
-
- 
